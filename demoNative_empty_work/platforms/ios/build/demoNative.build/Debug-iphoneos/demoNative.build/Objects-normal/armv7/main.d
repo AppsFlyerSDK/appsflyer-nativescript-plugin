f388a10a95a363e0e1376dd6cbcdba3f0c6964bb
Maxim Shoustin <maxim@appsflyer.com>
2016-11-20 10:54:59 +0200

    removed demo examples

--- demoNative_empty_work/platforms/ios/build/demoNative.build/Debug-iphoneos/demoNative.build/Objects-normal/armv7/main.d
+++
@@ -1,8 +0,0 @@
-dependencies: \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/main.m \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/TNSExceptionHandler.h \
-  /Users/maxim/Appsflyer/projects/nativescript-plugin-appsflyer/demoNative/platforms/ios/internal/TNSDebugging.h
--- demo_old/app/app.js
+++
@@ -1,4 +0,0 @@
-var application = require("application");
-application.cssFile = "main_style.css";
-
-application.start({ moduleName: "main-page" });
