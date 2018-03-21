20ab16003214d06cc68a8f63cac0600b7e0173a7
Dick Smith <richard.smith@ugroupmedia.com>
2018-03-21 15:56:19 -0400

    Update typings to include `setCustomerId`... ...and the types of the resolve objects, which are consistent. I left the reject object as any since iOS is consistent but Android returns the exceptions in some cases.

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,18 @@
 export interface InitSDKOptions {
-    devKey: string,
-    appId?: string,
-    isDebug?: boolean
+    devKey: string;
+    appId?: string;
+    isDebug?: boolean;
 }
 
 export interface TrackEventOptions {
-    eventName: string,
-    eventValues: Object
+    eventName: string;
+    eventValues: Object;
 }
 
 
 
-export function initSdk(options: InitSDKOptions): Promise<any>;
+export function initSdk(options: InitSDKOptions): Promise<{status: "success"}, any>;
 
-export function trackEvent(options:TrackEventOptions): Promise<any>;
+export function trackEvent(options: TrackEventOptions): Promise<{status: "success"}, any>;
+
+export function setCustomerUserId (userId: string): Promise<{status: "success"}, any>;
