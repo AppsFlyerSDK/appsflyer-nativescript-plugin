aebc8c2b95b8c88044592ea9c79113b9ae7c3aee
gabriel mandler <gabriel.mandler@appsflyer.com>
2023-07-25 17:49:04 +0300

    fix

--- demoNative/app/main-page.ts
+++ demoNative/app/main-page.ts
@@ -1,20 +1,7 @@
-import { EventData, Page, fromObject } from '@nativescript/core'
+import { EventData, Page } from '@nativescript/core'
 import { HelloWorldModel } from './main-view-model'
 
-export var viewModel =  fromObject({
-  gcdResponse: "GCD isn't called yet", 
-  logEventResponse: "not called yet", 
-  getAppsFlyerUIDResponse: "not called yet", 
-  initSdkResponse: "not initialized yet", 
-  logLocationResponse: [], 
-})
 export function navigatingTo(args: EventData) {
   const page = <Page>args.object
-
-  const newViewModel = new HelloWorldModel(viewModel)
-
-  viewModel.set("logEvent", function(){ return newViewModel.logEvent(viewModel)});
-
-  page.bindingContext = viewModel
-  newViewModel.initSdk();
+  page.bindingContext = new HelloWorldModel()
 }
--- demoNative/app/main-view-model.ts
+++ demoNative/app/main-view-model.ts
@@ -4,56 +4,63 @@ var appsFlyer = require("nativescript-plugin-appsflyer");
 
 export class HelloWorldModel extends Observable {
     options: any;
-    viewModel: Observable;
+    gcdResponse: string = "GCD isn't called yet";
+    logEventResponse: string = "not called yet";
+    getAppsFlyerUIDResponse: string = "not called yet";
+    initSdkResponse: string = "not initialized yet";
+    logLocationResponse = [];
 
-  constructor(viewModel: Observable) {
-    super()
+    constructor() {
+        super()
+        this.initSdk()
+    }
 
-    this.viewModel = viewModel
-    
-    this.options = {
-      devKey:  'WdpTVAcYwmxsaQ4WeTspmh',
-      appId: "975313579",
-      isDebug: true,
-      timeToWaitForATTUserAuthorization: 10,
-      onConversionDataSuccess: function(_res){
-          console.log("Get conversion data success: " + JSON.stringify(_res));
-          viewModel.set("gcdResponse", JSON.stringify(_res));
-      },
-      onConversionDataFail: function(_res){
-          console.log("Get conversion data failure: " + JSON.stringify(_res));
-          viewModel.set("gcdResponse", JSON.stringify(_res));
-      },
-      onAppOpenAttribution: function(_res){
-          console.log("onAppOpenAttribution: " + JSON.stringify(_res));
-      },
-      onAppOpenAttributionFailure: function(_res){
-          console.log("onAppOpenAttributionFailure: " + JSON.stringify(_res));
-      },
-      onDeepLinking: function(_res){
-        if(_res){
-            console.log("onDeepLinking: " + JSON.parse(_res).deepLink);
-        }
-      },
-    };
-  }
     initSdk(){
         console.log("call initSdk ... ");
+        const _this = this;
 
-        appsFlyer.initSdk(this.options).then(function(result) {
+        let options = {
+            devKey:  'WdpTVAcYwmxsaQ4WeTspmh',
+            appId: "975313579",
+            isDebug: true,
+            timeToWaitForATTUserAuthorization: 10,
+            onConversionDataSuccess: function(_res){
+                console.log("Get conversion data success: " + JSON.stringify(_res));
+                _this.set('gcdResponse', JSON.stringify(_res))
+            },
+            onConversionDataFail: function(_res){
+                console.log("Get conversion data failure: " + JSON.stringify(_res));
+                _this.set('gcdResponse', JSON.stringify(_res))
+            },
+            onAppOpenAttribution: function(_res){
+                console.log("onAppOpenAttribution: " + JSON.stringify(_res));
+            },
+            onAppOpenAttributionFailure: function(_res){
+                console.log("onAppOpenAttributionFailure: " + JSON.stringify(_res));
+            },
+            onDeepLinking: function(_res){
+              if(_res){
+                  console.log("onDeepLinking: " + JSON.parse(_res).deepLink);
+              }
+            },
+          };
+
+        appsFlyer.initSdk(options).then(function(result) {
             console.log("initSdk is called");
-            this.viewModel.set("initSdkResponse", result.status);
+            _this.set('initSdkResponse', result.status)
+
         }, function(err) {
             console.log("initSdk error  ... " +  JSON.stringify(err));
-            this.viewModel.set("initSdkResponse", JSON.stringify(err));
+            _this.set('initSdkResponse', JSON.stringify(err))
+
         });
     };
 
-     logEvent(viewModel : Observable) {
-
+     logEvent() {
         console.log("call logEvent ... ");
+        const _this = this;
 
-        var options = {
+        let options = {
             eventName: "af_add_to_cart",
             eventValues: {
                 af_content_id: "id123",
@@ -62,20 +69,20 @@ export class HelloWorldModel extends Observable {
             },
             onSuccess: function(_res){
                 console.log("onLogEventRequestSuccess results: " + JSON.stringify(_res));
-                viewModel.set("logEventResponse", JSON.stringify(_res));
+                _this.set("logEventResponse", JSON.stringify(_res));
             },
             onError: function(_res){
                 console.log("onLogEventRequestFailure results: " + JSON.stringify(_res));
-                viewModel.set("logEventResponse", JSON.stringify(_res));
+                _this.set("logEventResponse", JSON.stringify(_res));
             },
         };
 
         appsFlyer.logEvent(options).then(function(result) {
             console.log("logEvent is called");
-            viewModel.set("logEventResponse", JSON.stringify(result));
+            _this.set("logEventResponse", JSON.stringify(result));
         }, function(err) {
            console.log("logEvent ERROR results  ... " + JSON.stringify(err));
-           viewModel.set("logEventResponse", JSON.stringify(err));
+           _this.set("logEventResponse", JSON.stringify(err));
         });
     }
     
